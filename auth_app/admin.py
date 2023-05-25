4bb54a7f7908cc72d68e0cc88c1a7347a0fb49a7
Masud karim <msmasud578@gmail.com>
2023-05-25 16:23:19 +0600

    added Flights,Activities,Hotels,Packages,CustomPackages,Orders,Cancellations tables to the admin panel using admin.site.register() method

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Roles,UserModelExtended
+from .models import Roles,UserModelExtended,Flights,Activities,Hotels,Packages,CustomPackages,Orders,Cancellations
 
 # Register your models here.
 admin.site.register(Roles)
 admin.site.register(UserModelExtended)
+
+admin.site.register(Flights)
+admin.site.register(Activities)
+admin.site.register(Hotels)
+admin.site.register(Packages)
+admin.site.register(CustomPackages)
+admin.site.register(Orders)
+admin.site.register(Cancellations)
