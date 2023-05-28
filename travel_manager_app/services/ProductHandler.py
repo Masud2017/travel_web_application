cd8001cbbb57514ea765cacafacf2453a781bb14
Masud karim <msmasud578@gmail.com>
2023-05-29 00:24:32 +0600

    added created_at and updated_at also changed the field type for travel_date from DateField to DateTimeField

--- travel_manager_app/services/ProductHandler.py
+++ travel_manager_app/services/ProductHandler.py
@@ -2,8 +2,8 @@ from auth_app.models import Flights,UserModelExtended
 from django.contrib.auth.models import User
 
 class ProductHandler:
-    def __init__(self):
-        pass
+    def __init__(self,current_user):
+        self.current_user = current_user
 
     def add_flight(flight_from,flight_to,price,flight_image):
         Flights.objects.create()
--- travel_manager_app/views.py
+++ travel_manager_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib.auth.decorators import permission_required,login_required
 
+from .services.ProductHandler import ProductHandler
+
 # Create your views here.
 @login_required(login_url="/login")
 @permission_required([
@@ -12,4 +14,9 @@ def add_new_flight(request):
     flight_to = request.GET["to"]
     flight_image = request.GET["image"]
     price = request.GET["price"]
+
+    current_user = request.user
+    handler = ProductHandler(current_user)
+    handler.add_flight(flight_from,flight_to,price,flight_image)
+
     return HttpResponseRedirect("/")
